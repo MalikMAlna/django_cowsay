bdb8ee22c8916de0a291db9211efec77dfa2f408
Mike A <jimalik2525@gmail.com>
2020-05-11 17:23:34 -0400

    Did a bunch of stuff.

--- cowsave/views.py
+++ cowsave/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from cowsave.forms import AddTextForm
 
-# Create your views here.
+
+def index(request):
+    html = 'index.html'
+    form = AddTextForm(request.POST)
+
+    if request.method == "POST":
+        form = AddTextForm(request.POST)
+        if form.is_valid():
+            form.cleaned_data
+
+    return render(request, html, {"form": form})
--- cowsay/settings.py
+++ cowsay/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2459klt2&rfv#vo42dx(zhwky#k-ejwq@0*nmf-kmr+hw!-%vf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cowsave.apps.CowsaveConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'cowsay.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -86,16 +87,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+        + '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+            'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+            'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+            'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
