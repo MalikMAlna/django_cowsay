94ff2d2f0a77b165c584eeb8c26ec341b9165d8a
Mike A <jimalik2525@gmail.com>
2020-05-12 10:41:48 -0400

    Completed cowsay, the issue was with html and not formatting correctly with pre tags

--- cowsave/views.py
+++ cowsave/views.py
@@ -22,12 +22,7 @@ class HomeView(TemplateView):
             )
             cowsave = subprocess.check_output(
                 f'cowsay {text}', shell=True).decode("utf-8")
-            # cowsave_list = list(cowsave)
-            # cowsave_text = ""
-            # for num in cowsave_list:
-            #     cowsave_text += chr(num)
         form = AddTextForm()
-        # cowsave = cowsave_text.split("\n")
         args = {"form": form, 'cowsave': cowsave}
         return render(request, self.template_name, args)
 
